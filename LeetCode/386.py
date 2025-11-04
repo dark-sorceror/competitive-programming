e4b7c0bb37c15449ff6846b450a5e1d44496caa6
Hao Yan <haoyan160@gmail.com>
2025-11-04 16:14:51 -0500

    Problem 386 Attempt

---
+++ LeetCode/386.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/lexicographical-numbers/
+
+def lexicalOrder(n: int) -> list[int]:
+    q = []
+    c = 1
+
+    for i in range(n):
+        q.append(c)
+        
+        if c * 10 <= n:
+            c *= 10
+        
+        elif c % 10 != 9 and c + 1 <= n:
+            c += 1
+        
+        else:
+            while (c // 10) % 10 == 9 or c + 1 > n:
+                c //= 10
+                
+            c += 1
+
+    return q
