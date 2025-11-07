ca29e4da71b3b618b061ab4887082c00e7c8749e
Hao Yan <haoyan160@gmail.com>
2025-11-07 16:27:40 -0500

    Problem 2528 Attempt

---
+++ LeetCode/2528.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/maximize-the-minimum-powered-city/
+
+def maxPower(stations: list[int], r: int, k: int) -> int:
+    n, t, s = len(stations), [0], 0
+
+    for i in range(n):
+        s += stations[i]
+        t.append(s)
+        
+    p = [t[min(n, i + r + 1)] - t[max(0, i - r)] for i in range(n)]
+
+    def check(mp):
+        t = [0] * (n + 1)
+        a = 0
+        rem = k
+        
+        for i in range(n):
+            a += t[i]
+            c = p[i] + a
+            
+            if c < mp:
+                need = mp - c
+                
+                if (need) > rem: return False
+                
+                rem -= need
+                a += need
+                e = i + 2 * r + 1
+                
+                if e < n: t[e] -= need
+                    
+        return True
+
+    l, h, r = 0, sum(stations) + k, 0
+
+    while l <= h:
+        m = (l + h) // 2
+        
+        if check(m):
+            r = m
+            l = m + 1
+        else: h = m - 1
+
+    return r
