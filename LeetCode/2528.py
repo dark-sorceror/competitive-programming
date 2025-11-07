00cfed4557f7732f86eb19c5959e1e58fa68c1d0
Hao Yan <haoyan160@gmail.com>
2025-11-07 17:06:34 -0500

    Problem 2528 Solution

--- LeetCode/2528.py
+++ LeetCode/2528.py
@@ -7,15 +7,15 @@ def maxPower(stations: list[int], r: int, k: int) -> int:
         
         while l <= h:
             m = (l + h) // 2
-            needed = sum(max(0, m - i) for i in stations)
+            req = sum(max(0, m - i) for i in stations)
             
-            if needed <= k:
+            if req <= k:
                 r = m
                 l = m + 1
             else: h = m - 1
                 
         return r
-    
+
     n, t, s = len(stations), [0], 0
 
     for i in range(n):
@@ -24,7 +24,7 @@ def maxPower(stations: list[int], r: int, k: int) -> int:
         
     p = [t[min(n, i + r + 1)] - t[max(0, i - r)] for i in range(n)]
 
-    def check(mp):
+    def check(mp: int) -> bool:
         t = [0] * (n + 1)
         a = 0
         rem = k
@@ -40,20 +40,19 @@ def maxPower(stations: list[int], r: int, k: int) -> int:
                 
                 rem -= need
                 a += need
-                e = min(i + r, n -1) + r + 1
-                
-                if e < n: t[e] -= need
+                e = min(n, i + 2 * r + 1)
+                t[e] -= need
                     
         return True
 
-    l, h, r = 0, sum(stations) + k, 0
+    l, h, ans = 0, sum(stations) + k, 0
 
     while l <= h:
         m = (l + h) // 2
         
         if check(m):
-            r = m
+            ans = m
             l = m + 1
         else: h = m - 1
 
-    return r
+    return ans # (1361 ms)
