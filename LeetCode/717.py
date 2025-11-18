9988f77d6595bda0f63568167530f772ff301b45
Hao Yan <haoyan160@gmail.com>
2025-11-17 23:59:28 -0500

    Problem 717 Solution

---
+++ LeetCode/717.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/1-bit-and-2-bit-characters/
+def isOneBitCharacter(bits):
+    """
+    :type bits: List[int]
+    :rtype: bool
+    """
+
+    i, n = 0, len(bits)
+    
+    while i < n - 1:
+        if bits[i] == 1: i += 2
+        else: i += 1
+    
+    return i == n - 1 # (0 ms)
