81ce099f8f4c1c02f472dc3b771dc2efcb759ba9
Hao Yan <haoyan160@gmail.com>
2025-11-23 02:01:39 -0500

    Update Problem 1930 Solution

--- LeetCode/1930.py
+++ LeetCode/1930.py
@@ -1,15 +1,16 @@
+# https://leetcode.com/problems/unique-length-3-palindromic-subsequences/
+
 from string import ascii_lowercase
 
 def countPalindromicSubsequence(s: str) -> int:
     r = 0
 
     for i in ascii_lowercase:
-        f = s.find(i)
-        l = s.rfind(i)
+        f, l = s.find(i), s.rfind(i)
 
         if f != -1 and f < l:
             m = s[f + 1: l]
             
             r += len(set(m))
             
-    return r
+    return r # (147 ms)
