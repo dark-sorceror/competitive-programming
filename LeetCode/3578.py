c9f93aa1f3f2fa46c00b11a3955bc7ec16891d0a
Hao Yan <haoyan160@gmail.com>
2025-12-06 15:34:29 -0500

    Problem 3578 Attempt 2
    863/878 test cases passed

--- LeetCode/3578.py
+++ LeetCode/3578.py
@@ -9,11 +9,14 @@ def countPartitions(nums: list[int], k: int) -> int:
     for i in range(1, n + 1):
         c_max, c_min = nums[i - 1], nums[i - 1]
 
-        for j in range(i):
-            c_max = max(c_max, nums[j])
-            c_min = min(c_min, nums[j])
+        for j in range(i - 1, -1, -1):
+            if j < (i - 1):
+                c_max = max(c_max, nums[j])
+                c_min = min(c_min, nums[j])
 
             if (c_max - c_min) <= k:
                 vp[i] = (vp[i] + vp[j]) % (10**9 + 7)
+
+            else: break
                 
     return vp[n]
