a53ff23aacfbe0f14f974e3584547f86dc7cafc5
Hao Yan <haoyan160@gmail.com>
2025-12-07 02:05:13 -0500

    Problem 1523 Solution

--- LeetCode/1523.py
+++ LeetCode/1523.py
@@ -1,4 +1,4 @@
 # https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
 
 def countOdds(low: int, high: int) -> int:
-    return ((high - low) + 1) // 2 if low % 2 == 0 else ((high - low) + 1) // 2 + 1
+    return (high + 1) // 2 - low // 2 # (8 ms)
