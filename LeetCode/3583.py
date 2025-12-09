e209fbb1ed9d1090266f1dc5b5284e4c4d1d937d
Hao Yan <haoyan160@gmail.com>
2025-12-09 11:46:21 -0500

    Problem 3583 Solution

--- LeetCode/3583.py
+++ LeetCode/3583.py
@@ -1,17 +1,25 @@
 # https://leetcode.com/problems/count-special-triplets/
 
+from collections import Counter
+
 def specialTriplets(nums: list[int]) -> int:
     n, t = len(nums), 0
+    rc, lc = Counter(nums[2:]), Counter([nums[0]])
 
     for j in range(1, n - 1):
-        r, ci, ck = nums[j] * 2, 0, 0
+        r = nums[j] * 2
+
+        ci, ck = lc.get(r, 0), rc.get(r, 0)
+
+        if ci > 0 and ck > 0:
+            t = (t + (ci * ck) % (10**9 + 7)) % (10**9 + 7)
         
-        for i in range(j):
-            if nums[i] == r: ci += 1
+        lc[nums[j]] += 1
 
-        for k in range(j + 1, n):
-            if nums[k] == r: ck += 1
+        if j + 1 < n - 1:
+            nk = nums[j + 1]
+            rc[nk] -= 1
 
-        t = (t + (ci * ck) % (10**9 + 7)) % (10**9 + 7)
+            if rc[nk] == 0: del rc[nk]
 
-    return t
+    return t # (2022 ms)
