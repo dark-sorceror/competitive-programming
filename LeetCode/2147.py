d4d29d4f3b62810b0fd73ea48b47f634eed3ad32
Hao Yan <haoyan160@gmail.com>
2025-12-13 19:00:00 -0500

    Problem 2147 Solution

--- LeetCode/2147.py
+++ LeetCode/2147.py
@@ -4,14 +4,14 @@ def numberOfWays(corridor: str) -> int:
     s = [i for i, j in enumerate(corridor) if j == 'S']
     n = len(s)
 
-    t = 0
+    if n % 2 or n == 0: return 0
+
+    t = 1
     
     for i in range(1, n - 1, 2):
-        end = s[i]
-        start = s[i + 1]
-
+        end, start = s[i], s[i + 1]
         g = start - end
         
         t = (t * g) % (10**9 + 7)
         
-    return t
+    return t # (234 ms)
