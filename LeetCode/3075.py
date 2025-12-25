9d4825a1240a0ad542062f65f230900adc53a6f7
Hao Yan <haoyan160@gmail.com>
2025-12-24 20:24:04 -0700

    Problem 3075 Solution

--- LeetCode/3075.py
+++ LeetCode/3075.py
@@ -1,10 +1,11 @@
 # https://leetcode.com/problems/maximize-happiness-of-selected-children/
 
 def maximumHappinessSum(happiness: list[int], k: int) -> int:
+    happiness.sort(reverse=True)
+    
     t = 0
     
     for i in range(k):
-        h = happiness.pop(happiness.index(max(happiness)))
-        t += (max(0, h - (1 * i)))
+        t += (max(0, happiness[i] - i))
     
-    return t
+    return t # (824 ms)
