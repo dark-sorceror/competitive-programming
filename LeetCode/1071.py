179d63ef5df0bf881dfcf2ef355a773c52b21586
Hao Yan <haoyan160@gmail.com>
2025-12-28 16:23:10 -0700

    Problem 1071 Solution

---
+++ LeetCode/1071.py
@@ -0,0 +1,10 @@
+def gcdOfStrings(str1: str, str2: str) -> str:
+    if (str1 + str2) != (str2 + str1): return ""
+
+    def gcd(a: int, b: int):
+        while b:
+            a, b = b, a % b
+        
+        return a
+    
+    return str1[:gcd(len(str1), len(str2))]
