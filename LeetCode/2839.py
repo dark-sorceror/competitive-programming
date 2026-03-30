c45723ce4a5f5adf9da69850a08398e378914a67
Hao Yan <haoyan160@gmail.com>
2026-03-29 21:18:15 -0400

    Problem 2839 Solution

--- LeetCode/2839.py
+++ LeetCode/2839.py
@@ -1,7 +1,8 @@
 # https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
 
 def canBeEqual(s1: str, s2: str) -> bool:
-    if s1[0] == s2[2] and s1[1] == s2[3]:
-        return True
-        
-    return False
+    if sorted(s1[0] + s1[2]) == sorted(s2[0] + s2[2]):
+        if sorted(s1[1] + s1[3]) == sorted(s2[1] + s2[3]):
+            return True
+    
+    return False # (0 ms)
